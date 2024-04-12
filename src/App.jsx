63cda1df63110b1912dd14cc9fed37f70b831b04
JuliaMichaela <juliaperez@yandex.ru>
2024-04-12 22:31:11 +0300

    edited App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,19 @@ import heroes from './heroes.json';
 
 function App() {
   return (
-    <>
-
-
-    </>
+    <div className='heroes-container'>
+      {heroes.map((hero, index) => (
+        <Hero name={hero.name}
+          universe={hero.universe}
+          alterego={hero.alterego}
+          occupation={hero.occupation}
+          friends={hero.friends}
+          superpowers={hero.superpowers}
+          url={hero.url}
+          info={hero.info}
+          key={index} />
+      ))}
+    </div>
   )
 }
 
